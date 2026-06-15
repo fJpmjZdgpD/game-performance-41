6c5f76c20b6a38c7ac5e8c8f123d9439747443d1
Ethan Carter <kav_ispir@hotmail.com>
2026-06-15 04:09:16 -0400

    ➕ implement logging utility class

---
+++ logger.py
@@ -0,0 +1,32 @@
+import logging
+
+
+class GameLogger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+
+if __name__ == '__main__':
+    game_logger = GameLogger('GamePerformance')
+    game_logger.info('Logger initialized.')
