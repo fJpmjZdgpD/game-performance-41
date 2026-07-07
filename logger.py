e682b9c92c3d88df97eb96bed75637d1988188ff
Ethan Carter <kav_ispir@hotmail.com>
2026-07-07 04:09:14 -0400

    🔧 implement game logger utility function

--- logger.py
+++ logger.py
@@ -1,13 +1,34 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=10485760, backup_count=3):
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+class GameLogger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.FileHandler(f'{name}.log')
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
 
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+    def debug(self, message):
+        self.logger.debug(message)
 
-    logger.addHandler(handler)
-    return logger
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+# Example of usage
+if __name__ == '__main__':
+    logger = GameLogger('game_performance')
+    logger.info('Game started')
+    logger.debug('Debugging level information')
+    logger.warning('Warning! Low FPS detected')
+    logger.error('Error encountered during gameplay')
+    logger.critical('Critical error occurred')
