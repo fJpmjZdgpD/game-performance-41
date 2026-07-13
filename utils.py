048dec4836e6a49b7f0e25e0b83b04d9a7b6d9b1
Ethan Carter <kav_ispir@hotmail.com>
2026-07-13 04:09:13 -0400

    ➕ implement gaming data utility functions

--- utils.py
+++ utils.py
@@ -1,21 +1,25 @@
 import json
 
-def load_game_data(file_path):
-    with open(file_path, 'r') as file:
-        return json.load(file)
+class GameDataHandler:
+    @staticmethod
+    def load_data(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
 
+    @staticmethod
+    def save_data(file_path, data):
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
 
-def save_game_data(file_path, data):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4)
+    @staticmethod
+    def update_data(file_path, key, value):
+        data = GameDataHandler.load_data(file_path)
+        data[key] = value
+        GameDataHandler.save_data(file_path, data)
 
-
-def update_player_score(data, player_id, score):
-    if player_id in data:
-        data[player_id]['score'] += score
-    else:
-        data[player_id] = {'score': score}
-
-
-def get_top_players(data, top_n=5):
-    return sorted(data.items(), key=lambda x: x[1]['score'], reverse=True)[:top_n]
+    @staticmethod
+    def delete_data(file_path, key):
+        data = GameDataHandler.load_data(file_path)
+        if key in data:
+            del data[key]
+        GameDataHandler.save_data(file_path, data)
